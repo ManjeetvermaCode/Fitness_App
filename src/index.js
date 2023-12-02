31e07c3f563c7745d412bb173484fbff9bb30e33
manjeetverma <manjeetverma@gmail.com>
2023-12-02 22:12:03 +0530

    Done with basic app setup Also settedup assests and stylesheet

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//index.js is the file where we connect our project to the DOM
+import  ReactDOM from 'react-dom'
+
+import App from './App'
+const root=ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+    <App/>
+)
